4b931186032327a9c9644f5dcac4973535b7238b
Nicolas Lucino Epstein <nico@nilueps.net>
2020-05-08 15:34:07 -0400

    created a docs folder with compiled build

---
+++ docs/precache-manifest.59336e975c82931af96d9b756fd91ac4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2c46c566fd0195dc0437319b2f94133",
+    "url": "/fcc-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "31db3d2834d90a1ea38e",
+    "url": "/fcc-pomodoro-clock/static/css/main.438c3bbe.chunk.css"
+  },
+  {
+    "revision": "ad0db347d2c3bc58cd11",
+    "url": "/fcc-pomodoro-clock/static/js/2.303aab4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fcc-pomodoro-clock/static/js/2.303aab4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31db3d2834d90a1ea38e",
+    "url": "/fcc-pomodoro-clock/static/js/main.4a993816.chunk.js"
+  },
+  {
+    "revision": "477ff0ee8f460ba281ae",
+    "url": "/fcc-pomodoro-clock/static/js/runtime-main.bb1eafb4.js"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/fcc-pomodoro-clock/static/media/Roboto-Thin.321de678.ttf"
+  },
+  {
+    "revision": "45dd5ea12786135dc454fc7b37035039",
+    "url": "/fcc-pomodoro-clock/static/media/bell.45dd5ea1.ogg"
+  },
+  {
+    "revision": "204def9617100e67624eb9fb981282f9",
+    "url": "/fcc-pomodoro-clock/static/media/deepbell.204def96.ogg"
+  }
+]);
